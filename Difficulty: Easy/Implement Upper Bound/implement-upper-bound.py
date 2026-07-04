d77975529c447e01b2674465c30656de2880457d
Rajat kumar <rajat910260@gmail.com>
2026-07-04 21:10:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.py
@@ -0,0 +1,18 @@
+class Solution:
+    def upperBound(self, arr, target):
+        # code here
+     
+        low = 0
+        high = len(arr) - 1
+        ans = len(arr)
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if arr[mid] > target:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
