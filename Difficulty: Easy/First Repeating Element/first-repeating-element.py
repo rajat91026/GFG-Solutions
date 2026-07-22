428dfdaf465ae4e09ebc2d9a87cd0747dc79ea53
Rajat kumar <rajat910260@gmail.com>
2026-07-22 18:50:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Repeating Element/first-repeating-element.py
+++ Difficulty: Easy/First Repeating Element/first-repeating-element.py
@@ -1,16 +1,14 @@
 class Solution:
     def firstRepeated(self, arr):
         freq = {}
-
-    
         for num in arr:
-            freq[num] = freq.get(num, 0) + 1
-
-        
+            freq[num]=freq.get(num,0)+1
         for i in range(len(arr)):
-            if freq[arr[i]] > 1:
-                return i + 1    
-
-        return -1
-    
+            if freq[arr[i]]>1:
+                return i+1
+            
+        return -1   
+         
+            
+         
         
