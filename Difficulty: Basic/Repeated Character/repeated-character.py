6873ea02de444420f333bd2f50c83e54e8d62ca2
Rajat kumar <rajat910260@gmail.com>
2026-09-03 22:23:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstRep(self, s):
+        freq = {}
+    
+        
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+    
+        
+        for ch in s:
+            if freq[ch] > 1:
+                return ch
+    
+        return -1
